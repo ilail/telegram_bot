af5a7a7561abab2fd3572059e53229b9a92b2f65
Viktoriya Loginova <torilogin98@gmail.com>
2017-06-04 21:25:02 +0300

    'working'

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
-access_token = '343385816:AAFyEH-1OAn9Bl05EGrmeCCuffYk8BboG6A'
-domain = 'http://www.ifmo.ru/ru/schedule/0'
-day_in_week={'monday':'1day','tuesday':'2day',\
-          'wednesday':'3day','thursday':'4day',\
-          'friday':'5day','saturday':'6day',\
-          'sunday':'1day'}
+access_token = '391248410:AAGDpdSETw8jbNsfvLr7aurCsb3cholrH6k'
 
-week_list = {'even': 1,'odd': 2,'both': 0}
+domain = 'http://www.ifmo.ru/ru/schedule/0'
+
+day_in_week = {'/monday':'1day','/tuesday':'2day','/wednesday':'3day',\
+               '/thursday':'4day','/friday':'5day','/saturday':'6day',\
+               '/sunday':'1day'}
+
+week = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+
+week_list = {'even': 1,'odd': 2}
--- tg.py
+++ tg.py
@@ -10,12 +10,15 @@ bot = telebot.TeleBot(config.access_token)
 
 
 def get_page(group, week=''):
+    week = None if 0 else week
     if week:
         week = str(week) + '/'
     url = '{domain}/{group}/{week}raspisanie_zanyatiy_{group}.htm'.format(
         domain=config.domain,
         week=week,
-        group=group)
+        group=group
+    )
+    print(url)
     response = requests.get(url)
     web_page = response.text
     return web_page
@@ -25,22 +28,23 @@ def get_schedule(web_page, day):
     soup = BeautifulSoup(web_page, "html5lib")
     schedule_table = soup.find("table", attrs={"id": day})
     # Время проведения занятий
+    print(schedule_table)
     times_list = schedule_table.find_all("td", attrs={"class": "time"})
     times_list = [time.span.text for time in times_list]
     # Место проведения занятий
     locations_list = schedule_table.find_all("td", attrs={"class": "room"})
-    locations_list = [room.span.text for room in locations_list]     
+    locations_list = [room.span.text for room in locations_list]
+    # Аудитории проведения занятий
+    hall_list = schedule_table.find_all("dd", attrs={"class": "rasp_aud_mobile"})
+    hall_list = [hall.text for hall in hall_list]      
     # Название дисциплин и имена преподавателей
     lessons_list = schedule_table.find_all("td", attrs={"class": "lesson"})
     lessons_list = [lesson.text.split('\n\n') for lesson in lessons_list]
     lessons_list = [', '.join([info for info in lesson_info if info]) for lesson_info in lessons_list]
-    # Аудитории проведения занятий
-    hall_list = schedule_table.find_all("dd", attrs={"class": "rasp_aud_mobile"})
-    hall_list = [hall.text for hall in hall_list] 
-    return times_list, locations_list, lessons_list, hall_list
+    return times_list, locations_list, lessons_list
 
 
-def week_and_day(week_n, day_n):
+def what_week(week_n, day_n):
     week = 1 if week_n % 2 else 2
     day = '1day' if '7day' else day_n
     if day_n == '7day' and week == 1:
@@ -48,21 +52,21 @@ def week_and_day(week_n, day_n):
     else: week = 1
     return week, day
 
-
+#(commands=list(config.day_in_week.keys()))
 @bot.message_handler(commands=['monday','tuesday','wednesday','thursday','friday','saturday','sunday'])
 def get_exact_day(message):
+    print(message)
     day, week, group = message.text.split()
     if day in config.day_in_week.keys():
-        day_n = day
         day = config.day_in_week.get(day)
-    try:
-        web_page = get_page(group, week)
-        times_list, locations_list, lessons_list, hall_list = get_schedule(web_page, day)
-        resp = ''
-        for time, location, lesson, hall in zip(times_list, locations_list, lessons_list, hall_list):
-            resp += '<b>{}, {}, {}</b>, {}\n'.format(time, hall, location, lesson)
-    except:
-        resp = 'Занятий в этот день ({}, неделя {}) нет.'.format(day_n, week)
+    print(group)
+    print(day)
+    web_page = get_page(group, week)
+    #print(web_page)
+    times_list, locations_list, lessons_list = get_schedule(web_page, day)
+    resp = ''
+    for time, location, lesson in zip(times_list, locations_list, lessons_list):
+        resp += '<b>{}, {}</b>, {}\n'.format(time, location, lesson)
     bot.send_message(message.chat.id, resp, parse_mode='HTML')
 
 
@@ -70,13 +74,13 @@ def get_exact_day(message):
 def get_tomorrow(message):
     _, group = message.text.split()
     n = datetime.now().isocalendar()  #(0-2017,week-22,day-7)
-    tomorrow = week_and_day(n[1], str(n[2])+'day')
+    tomorrow = what_week(n[1], str(n[2])+'day')
     week, day = tomorrow[0], tomorrow[1]
     web_page = get_page(group, week)
     times_list, locations_list, lessons_list, hall_list = get_schedule(web_page, day)
     resp = ''
-    for time, location, lesson, hall in zip(times_list, locations_list, lessons_list, hall_list):
-        resp += '<b>{}, {}, {}</b>, {}\n'.format(time, hall, location, lesson)
+    for time, location, lesson in zip(times_list, locations_list, lessons_list):
+        resp += '<b>{}</b>, {},{}\n'.format(time, location, lesson)
     bot.send_message(message.chat.id, resp, parse_mode='HTML')
 
 
@@ -84,21 +88,19 @@ def get_tomorrow(message):
 def get_all_week(message):
     _, week, group = message.text.split()
     if week in config.week_list.values(): 
-        week = week
+        num_week = week
     elif week in config.week_list.keys():
-        week = config.week_list.get(week) 
-    web_page = get_page(group, week)
+        num_week = config.week_list.get(week) 
+    web_page = get_page(group, num_week)
     resp = ''
     for d in range(1,7):
         day = str(d)+'day'
-        try:
-            times_list, locations_list, lessons_list, hall_list = get_schedule(web_page, day)
-            day = config.week[d-1].upper()
-        except:
-            continue
-        resp += '\n\n<b>'+day+'</b>\n'
+        times_list, locations_list, lessons_list, hall_list = get_schedule(web_page, day)
+        if day in config.day_in_week.values():
+            day_verb = config.day_in_week.key(day)
+        resp += '\n\n'+day+'\n'
         for time, location, lesson, hall in zip(times_list, locations_list, lessons_list, hall_list):
-            resp += '<b>{}, {}, {}</b>, {}\n'.format(time, hall, location, lesson)
+            resp += '<b>{}</b>, {}, {},{}\n'.format(time, hall, location, lesson)
     bot.send_message(message.chat.id, resp, parse_mode='HTML')
   
 
@@ -107,20 +109,17 @@ def get_near_lesson(message):
     _, group = message.text.split()
     date = datetime.now().isocalendar()
     week_n, day_n = date[1], date[2]
-    week, day = week_and_day(week_n, day_n)
+    week, day = what_week(week_n, day_n)
     current_time = datetime.strftime(datetime.now(), "%H:%M")
+    class_time = datetime.strftime(datetime.strptime(time[:4],"%H:%M"),"%H:%M")
     web_page = get_page(group, week)
     times_list, locations_list, lessons_list, hall_list = get_schedule(web_page, day)
     resp = ''
-    resp += "<b>Время: {}</b>\n<Пара: \n".format(current_time)
+    resp += "<b>Сейчас: {}</b>\n Ближайшая пара: \n".format(current_time)
     for time, location, lesson, hall in zip(times_list, locations_list, lessons_list, hall_list):
-        try:
-            class_time = datetime.strftime(datetime.strptime(time[:4],"%H:%M"),"%H:%M")
-            if class_time > current_time:
-                resp += '<b>{}, {}, {}</b>, {}\n'.format(time, hall, location, lesson)
-                break
-        except:
-            resp = 'Пар в ближайшее время нет! Либо они "днём".'
+        if class_time > current_time:
+            resp += '<b>{}</b>, {}, {}\n'.format(time, location, lession)
+            break
     bot.send_message(message.chat.id, resp, parse_mode='HTML')
 
 
